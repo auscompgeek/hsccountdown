393d327350f9fd8bb5ea5e6fadb95519a2c6412c
David <auscompgeek@users.noreply.github.com>
2014-12-11 16:05:03 +1100

    data.js: turns out I don't know how to count

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 var things = {
-	"2014-12-17T17:00:00Z": "HSC results are released",
-	"2014-12-18T20:00:00Z": "ATARs are released"
+	"2014-12-16T19:00:00Z": "HSC results are released",
+	"2014-12-17T22:00:00Z": "ATARs are released"
 }
