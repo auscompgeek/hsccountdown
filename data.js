29d9af9e1913dac6fe123794fe8587006746eb0a
David <auscompgeek@users.noreply.github.com>
2015-04-20 21:21:52 +1000

    data: 2015 dates

--- data.js
+++ data.js
@@ -1,4 +1,6 @@
 var things = {
-	"2014-12-16T19:00:00Z": "HSC results are released",
-	"2014-12-17T22:00:00Z": "ATARs are released"
+	"2015-10-12T21:20:00Z": "HSC written exams commence",
+	"2015-11-07T03:00:00Z": "No more HSC exams!",
+	"2015-12-16T19:00:00Z": "HSC results are released",
+	"2015-12-17T22:00:00Z": "ATARs are released"
 }
