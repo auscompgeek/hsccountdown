cad767b7aea957faedfa7fb9f601df8461fc70e9
David <auscompgeek@users.noreply.github.com>
2015-10-11 19:29:04 +1100

    data: fix dates, add first two days of exams

--- data.js
+++ data.js
@@ -1,6 +1,16 @@
-var things = {
-	"2015-10-12T21:20:00Z": "HSC written exams commence",
-	"2015-11-07T03:00:00Z": "No more HSC exams!",
-	"2015-12-16T19:00:00Z": "HSC results are released",
-	"2015-12-17T22:00:00Z": "ATARs are released"
-}
+var events = {
+	"2015-10-11T23:20Z": "HSC written exams commence",
+	"2015-11-04T07:00Z": "No more HSC exams!",
+	"2015-12-16T19:00Z": "HSC results are released",
+	"2015-12-17T22:00Z": "ATARs are released"
+};
+
+var exams = {
+	"2015-10-11T23:20Z": "English Paper 1",
+	"2015-10-12T03:00Z": "Japanese Beginners; Modern Hebrew",
+	"2015-10-12T22:25Z": "English Paper 2",
+	"2015-10-13T02:55Z": "Music 1",
+	"2015-10-13T04:25Z": "Music 2",
+};
+
+var things = events;
