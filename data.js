b4b8ef68edc08ad50acb407f5abc6778105d29e2
David <auscompgeek@users.noreply.github.com>
2015-10-11 20:09:22 +1100

    data: add the rest of week 1

--- data.js
+++ data.js
@@ -7,10 +7,16 @@ var events = {
 
 var exams = {
 	"2015-10-11T23:20Z": "English Paper 1",
-	"2015-10-12T03:00Z": "Japanese Beginners; Modern Hebrew",
+	"2015-10-12T03:00Z": "Japanese Beginners, Modern Hebrew",
 	"2015-10-12T22:25Z": "English Paper 2",
 	"2015-10-13T02:55Z": "Music 1",
 	"2015-10-13T04:25Z": "Music 2",
+	"2015-10-13T22:25Z": "Dance, IPT, Latin, Indonesian Beginners/Continuers",
+	"2015-10-14T02:55Z": "Society and Culture, Engineering Studies, VET Financial Services, VET Primary Industries",
+	"2015-10-14T22:25Z": "Ancient History, Arabic Ext, Classical Hebrew Ext, Indonesian Background, Japanese Background",
+	"2015-10-15T02:55Z": "Earth and Env Science, Japanese Ext, German Beginners/Continuers, VET Retail Services",
+	"2015-10-15T22:25Z": "Economics, Spanish Ext",
+	"2015-10-16T02:55Z": "PDHPE, Latin Ext, Heritage Indonesian",
 };
 
 var things = events;
