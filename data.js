ec306a54cf567b3a109c3b27f00d2a092004e0c1
David <auscompgeek@users.noreply.github.com>
2015-10-15 21:41:56 +1100

    data: make dates AEDT
    Just to keep @evan950 happy.

--- data.js
+++ data.js
@@ -6,17 +6,17 @@ var events = {
 };
 
 var exams = {
-	"2015-10-11T23:20Z": "English Paper 1",
-	"2015-10-12T03:00Z": "Japanese Beginners, Modern Hebrew",
-	"2015-10-12T22:25Z": "English Paper 2",
-	"2015-10-13T02:55Z": "Music 1",
-	"2015-10-13T04:25Z": "Music 2",
-	"2015-10-13T22:25Z": "Dance, IPT, Latin, Indonesian Beginners/Continuers",
-	"2015-10-14T02:55Z": "Society and Culture, Engineering Studies, VET Financial Services, VET Primary Industries",
-	"2015-10-14T22:25Z": "Ancient History, Arabic Ext, Classical Hebrew Ext, Indonesian Background, Japanese Background",
-	"2015-10-15T02:55Z": "Earth and Env Science, Japanese Ext, German Beginners/Continuers, VET Retail Services",
-	"2015-10-15T22:25Z": "Economics, Spanish Ext",
-	"2015-10-16T02:55Z": "PDHPE, Latin Ext, Heritage Indonesian",
+	"2015-10-12T10:20+1100": "English Paper 1",
+	"2015-10-12T14:00+1100": "Japanese Beginners, Modern Hebrew",
+	"2015-10-13T09:25+1100": "English Paper 2",
+	"2015-10-13T13:55+1100": "Music 1",
+	"2015-10-13T15:25+1100": "Music 2",
+	"2015-10-14T09:25+1100": "Dance, IPT, Latin, Indonesian Beginners/Continuers",
+	"2015-10-14T13:55+1100": "Society and Culture, Engineering Studies, VET Financial Services, VET Primary Industries",
+	"2015-10-15T09:25+1100": "Ancient History, Arabic Ext, Classical Hebrew Ext, Indonesian Background, Japanese Background",
+	"2015-10-15T13:55+1100": "Earth and Env Science, Japanese Ext, German Beginners/Continuers, VET Retail Services",
+	"2015-10-16T09:25+1100": "Economics, Spanish Ext",
+	"2015-10-16T13:55+1100": "PDHPE, Latin Ext, Heritage Indonesian",
 };
 
 var things = events;
