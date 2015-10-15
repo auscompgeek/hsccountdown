22b05fe5e16551ac569f090f9d06ef51a299763c
David <auscompgeek@users.noreply.github.com>
2015-10-15 21:42:17 +1100

    data: add week 2

--- data.js
+++ data.js
@@ -17,6 +17,16 @@ var exams = {
 	"2015-10-15T13:55+1100": "Earth and Env Science, Japanese Ext, German Beginners/Continuers, VET Retail Services",
 	"2015-10-16T09:25+1100": "Economics, Spanish Ext",
 	"2015-10-16T13:55+1100": "PDHPE, Latin Ext, Heritage Indonesian",
+	"2015-10-19T09:25+1100": "Biology",
+	"2015-10-19T13:55+1100": "VET Hospo, Classical Hebrew, Indonesian Ext, Japanese Continuers",
+	"2015-10-20T09:25+1100": "English Ext, Arabic, VET Business Services, VET Automotive",
+	"2015-10-20T13:55+1100": "Agriculture, a lot of languages",
+	"2015-10-21T09:25+1100": "Maths General 2",
+	"2015-10-21T13:55+1100": "Maths",
+	"2015-10-22T09:25+1100": "Legal Studies, Chinese Beginners/Background",
+	"2015-10-22T13:55+1100": "Senior Science, German Ext",
+	"2015-10-23T09:25+1100": "Chemistry, Aboriginal Studies, VET Tourism",
+	"2015-10-23T13:55+1100": "CAFS, SDD, Classical Greek, Heritage Korean",
 };
 
 var things = events;
